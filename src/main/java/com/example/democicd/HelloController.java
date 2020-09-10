f1295a61444dd98b2d4282cec8e5e336dbfbefbf
Bicky Choudhary <bicky.choudhary@wipro.com>
2020-09-10 10:17:22 +0530

    msg change

--- src/main/java/com/example/democicd/HelloController.java
+++ src/main/java/com/example/democicd/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 	
 	@GetMapping
 	public String getMessage() {
-		return "Hello from Sprin Boot !!";
+		return "Hello from Sprin Boot Using CICD pipeline!!";
 	}
 
 }
